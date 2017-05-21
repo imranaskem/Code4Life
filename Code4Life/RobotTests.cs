91caa49b53f4d1c10821ee6404b3358e1bc998dc
imranaskem <imran.askem@gmail.com>
2017-05-21 22:31:09 +0100

    Initial commit

---
+++ Code4Life/RobotTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+
+[TestFixture]
+public class RobotTests
+{
+    private int[] molecules;
+    private Robot robot;
+
+    [SetUp]
+    public void SetupTest()
+    {
+        this.molecules = = new int[] { 1, 2, 3, 4, 5 };
+        this.robot = new Robot("START_POS", 0, 0, this.molecules);
+    }
+
+    [Test]
+    public void RobotSetup()
+    {     
+        Assert.That(robot.Position, Is.EqualTo(Position.StartPos));
+        Assert.That(robot.Eta, Is.EqualTo(0));
+        Assert.That(robot.Score, Is.EqualTo(0));
+
+        foreach (var kvp in robot.Storage)
+        {
+            Assert.That(kvp.Value, Is.EqualTo(molecules[(int)kvp.Key]));
+        }
+    }
+}
---
+++ Code4Life/SampleTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+
+[TestFixture]
+public class SampleTests
+{
+    private int[] molecules;
+    private Sample sample;
+
+    [SetUp]
+    public void SetupSample()
+    {
+        this.molecules = new int[] { 1, 0, 1, 1, 1 };
+        this.sample = new Sample(1, 1, 10, molecules);
+    }
+
+    [Test]
+    public void SampleSetup()
+    {      
+        Assert.That(sample.Id, Is.EqualTo(1));
+        Assert.That(sample.CarriedBy, Is.EqualTo(Carried.Other));
+        Assert.That(sample.HealthPoints, Is.EqualTo(10));
+        Assert.That(sample.MoleculeValue, Is.EqualTo(2.5));
+
+        foreach (var kvp in sample.Cost)
+        {
+            Assert.That(kvp.Value, Is.EqualTo(molecules[(int)kvp.Key]));
+        }
+    }
+}
