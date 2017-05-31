f3ab3874aac60f63dc3cc3fd83c4b514e5a0276b
Imran Askem <imran.askem@gmail.com>
2017-05-31 09:40:54 +0100

    Started AI tests

---
+++ Code4Life/AiTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+[TestFixture]
+public class AiTests
+{
+    private AI ai;
+
+    [SetUp]
+    public void SetupAi()
+    {
+        var robot = new Robot("START_POS", 0, 0, new int[] { 0, 0, 0, 0, 0 });
+        this.ai = new AI(robot);
+    }
+
+    [Test]
+    public void SetupTest()
+    {
+        Assert.That(this.ai.Mode, Is.EqualTo(Mode.SamplePickup));
+    }
+}
+
--- Code4Life/MoleculeBagTests.cs
+++ Code4Life/MoleculeBagTests.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
-using NUnit.Framework;
+using NUnit.Framework;
+
 [TestFixture]
 public class MoleculesBagTests
 {
@@ -11,7 +12,8 @@ public class MoleculesBagTests
     public void SetupMoleculeBag()
     {
         this.moleculeBag = new MoleculeBag();
-    }
+    }
+
     [Test]
     public void MoleculeBagSetup()
     {      
@@ -19,17 +21,23 @@ public class MoleculesBagTests
         {
             Assert.That(item.Value, Is.EqualTo(0));
         }
-    }
+    }
+
     [Test]
     public void UpdateMoleculeValuesTest()
     {
-        var moleculeValues = new int[] { 2, 2, 2, 2, 2 };
-        this.moleculeBag.UpdateMoleculeValues(moleculeValues);
+        var moleculeValues = new int[] { 2, 2, 2, 2, 2 };
+
+        this.moleculeBag.UpdateMoleculeValues(moleculeValues);
+
         foreach (var item in this.moleculeBag)
         {
             Assert.That(item.Value, Is.EqualTo(moleculeValues[(int)item.Key]));
         }
-    }    [Test]    public void UpdateSingleValueTest()
+    }
+
+    [Test]
+    public void UpdateSingleValueTest()
     {
         this.moleculeBag.UpdateSingleValue(MoleculeType.A, 2);
         this.moleculeBag.UpdateSingleValue(MoleculeType.A, 2);
@@ -37,7 +45,11 @@ public class MoleculesBagTests
         var molecule = this.moleculeBag.Single(s => s.Key == MoleculeType.A);
 
         Assert.That(molecule.Value, Is.EqualTo(4));
-    }    [Test]    public void SetValuesToZeroTest()    {
+    }
+
+    [Test]
+    public void SetValuesToZeroTest()
+    {
         this.moleculeBag.UpdateSingleValue(MoleculeType.A, 2);
         this.moleculeBag.UpdateSingleValue(MoleculeType.A, 2);
 
@@ -47,30 +59,41 @@ public class MoleculesBagTests
         {
             Assert.That(item.Value, Is.EqualTo(0));
         }
-    }    [Test]    public void GetValueOfKeyTest()
+    }
+
+    [Test]
+    public void GetValueOfKeyTest()
     {
         var value = this.moleculeBag.GetValueOfKey(MoleculeType.A);
 
         Assert.That(value, Is.EqualTo(0));
-    }
+    }
+
     [Test]
     public void UpdateMoleculeValuesExceptionTest()
     {
-        var moleculeValues = new int[] { 5, 2, 2, 2, 2 };
+        var moleculeValues = new int[] { 5, 2, 2, 2, 2 };
+
         Assert.That(() => this.moleculeBag.UpdateMoleculeValues(moleculeValues), Throws.TypeOf<ArgumentOutOfRangeException>());    
-    }
+    }
+
     [Test]
     public void EmptyRemainingCapacityTest()
     {
-        var capacity = this.moleculeBag.RemainingCapacity();
+        var capacity = this.moleculeBag.RemainingCapacity();
+
         Assert.That(capacity, Is.EqualTo(10));
-    }
+    }
+
     [Test]
     public void FullRemainingCapacityTest()
     {
-        var moleculeValues = new int[] { 2, 2, 2, 2, 2 };
-        this.moleculeBag.UpdateMoleculeValues(moleculeValues);
-        var capacity = this.moleculeBag.RemainingCapacity();
+        var moleculeValues = new int[] { 2, 2, 2, 2, 2 };
+
+        this.moleculeBag.UpdateMoleculeValues(moleculeValues);
+
+        var capacity = this.moleculeBag.RemainingCapacity();
+
         Assert.That(capacity, Is.EqualTo(0));
     }
 }
